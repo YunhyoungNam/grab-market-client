0f8d3ee8f8a2dc243a9b419f47c3aab4cf094088
unknown <darkstar0983@snu.ac.kr>
2022-09-21 12:53:45 +0900

    상품상세페이지

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
+import "antd/dist/antd.css";
 import "./App.css";
 import MainPage from "./main";
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import UploadPage from "./upload";
 import ProductPage from "./product";
+import { Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 
 function App() {
+  const navigate = useNavigate();
   return (
     <div>
-      <Routes>
-        <Route path={"/"} element={<MainPage />} />
-        <Route path={"/upload"} element={<UploadPage />} />
-        <Route path={"/product/:id"} element={<ProductPage />} />
-      </Routes>
+      <div id="header">
+        <div id="header-area">
+          <Link to="/">
+            <img src="/images/icons/logo.png" />
+          </Link>
+          <Button
+            size="large"
+            onClick={function() {
+              navigate("/upload");
+            }}
+            icon={<DownloadOutlined />}
+          >
+            상품 업로드
+          </Button>
+        </div>
+      </div>
+      <div id="body">
+        <Routes>
+          <Route path={"/"} element={<MainPage />} />
+          <Route path={"/upload"} element={<UploadPage />} />
+          <Route path={"/product/:id"} element={<ProductPage />} />
+        </Routes>
+      </div>
+      <div id="footer"></div>
     </div>
   );
 }
--- src/product/index.js
+++ src/product/index.js
@@ -1,10 +1,48 @@
-import React from "react";
+import "./index.css";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function ProductPage() {
   const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  useEffect(function() {
+    axios
+      .get(
+        "https://382da1d7-8462-4779-8fc6-27c4f4e956a7.mock.pstmn.io/products/" +
+          id
+      )
+      .then(function(result) {
+        setProduct(result.data);
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
+  }, []);
 
-  return <h1>상품상세 페이지 {id}</h1>;
+  if (product === null) {
+    return <h1>상품 정보를 받고 있습니다.</h1>;
+  }
+  console.log(id);
+  console.log(product);
+  console.log(product.products[id].name);
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.products[id].imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.products[id].seller}</span>
+      </div>
+      <div id="contents-box">
+        <div id="name">{product.products[id].name}</div>
+        <div id="price">{product.products[id].price + "원"}</div>
+        <div id="createdAt">2022년 09월 20일</div>
+        <div id="description">{product.products[id].discription}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
